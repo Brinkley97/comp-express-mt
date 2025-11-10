5ec0227557001aabef07888deaae80e561975b64
KwekuYamoah <kwekuandyamoah@gnail.com>
2025-11-10 12:31:08 -0500

    Add experiment name parameter to run_zero_shot_experiment; enhance output logging and save results to JSON file

--- experiments/experiment_a.py
+++ experiments/experiment_a.py
@@ -30,13 +30,14 @@ def load_json(filepath: str) -> Dict:
         data = json.load(file)
     return data
 
-def run_zero_shot_experiment(model_name: List[str], dataset: Dict, source_lang='akan', target_lang='english', prompts: [ZeroShotPromptFactory, FewShotPromptFactory, ChainOfThoughtPrompt]=ZeroShotPromptFactory) ->Dict:
+def run_zero_shot_experiment(model_name: List[str], dataset: Dict, source_lang='akan', target_lang='english', prompts: [ZeroShotPromptFactory, FewShotPromptFactory, ChainOfThoughtPrompt]=ZeroShotPromptFactory, experiment_name=None) ->Dict:
     outputs = {}
 
     # Initialize the text generation model
     for current_model in model_name:
-        print(f"Running Zero Shot experiment with {current_model}",
-              f"\n**********************************************\n")
+        print(f"\n***********************************************************",
+              f"Running Zero Shot experiment with {current_model}",
+              f"\n***********************************************************\n")
         llm_instance = TextGenerationModelFactory
         model = llm_instance.create_instance(current_model)
 
@@ -69,7 +70,13 @@ def run_zero_shot_experiment(model_name: List[str], dataset: Dict, source_lang='
     
         # initialize output dicts with results for each model
         outputs[current_model] = results
-        print(f"Sample results for {current_model}: {results}")
+        
+    # write to json file
+    if experiment_name is not None:
+        output_path = f"experiments/results/{experiment_name}_zero_shot_results.json"
+        with open(output_path, 'w', encoding='utf-8') as f:
+            json.dump(results, f, ensure_ascii=False, indent=4)
+    return outputs
 
     #     # Generate the model's response
     #     response = model.generate_text(prompt)
@@ -100,4 +107,4 @@ if __name__ == "__main__":
 
     zero_shot_direct_prompt = ZeroShotPromptFactory("direct")
 
-    run_zero_shot_experiment(model_name=selected_models, dataset=dataset_dict, prompts=zero_shot_direct_prompt)
+    run_zero_shot_experiment(model_name=selected_models, dataset=dataset_dict, prompts=zero_shot_direct_prompt, experiment_name="1_to_many_experiment_a")
